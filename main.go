327d729c40db1825a611462f2973d3b560b3f10d
kencx <kennethcheo@pm.me>
2022-08-15 22:08:32 +0800

    implement concurrent requests

--- main.go
+++ main.go
@@ -2,93 +2,108 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"net/http"
 	"net/url"
-	"os"
 	"path"
 	"strconv"
+	"sync"
 	"time"
 )
 
+// full endpoint: http://xkcd.com/<comic-num>/info.0.json
+var (
+	baseUrl  = "http://xkcd.com"
+	endPoint = "info.0.json"
+)
+
 type Comic struct {
-	Title      string
-	Number     int `json:"num"`
-	Alt        string
-	Img        string `json:"img"`
+	Title      string `json:"title"`
+	SafeTitle  string `json:"safe_title,omitempty"`
+	Number     int    `json:"num"`
+	Alt        string `json:"alt,omitempty"`
+	ImgUrl     string `json:"img"`
 	Transcript string `json:"transcript,omitempty"`
+	Day        string `json:"day"`
+	Month      string `json:"month"`
+	Year       string `json:"year"`
 }
 
-func GetComic(num string) Comic {
-	baseUrl := "http://xkcd.com"
-	url, err := getAPIEndpoint(baseUrl, num)
+// TODO safe cancellation
+func GetAllComics(total int) ([]*Comic, error) {
 
+	var wg sync.WaitGroup
+	ch := make(chan struct{}, 50) // max number of concurrent requests
+	comics := make([]*Comic, total)
+
+	for i := 1; i < total+1; i++ {
+		wg.Add(1)
+		s := strconv.Itoa(i)
+
+		go func(i int, num string) {
+			defer wg.Done()
+			ch <- struct{}{} // acquire token
+			comic, err := GetComic(num)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			comics[i-1] = comic
+			<-ch // release token
+		}(i, s)
+	}
+
+	wg.Wait()
+	return comics, nil
+}
+
+func GetComic(num string) (*Comic, error) {
+	url, err := parseEndpoint(baseUrl, num)
 	if err != nil {
-		log.Fatalf("URL invalid: %v", err)
-		os.Exit(1)
+		return nil, err
 	}
 
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("http GET failed: %v", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("request to %v failed: %v", url, err)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
 	if err != nil {
-		log.Fatalf("ReadAll failed: %v", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("failed to read response: %v", err)
 	}
+	defer resp.Body.Close()
 
 	var comic Comic
-	if err := json.Unmarshal([]byte(body), &comic); err != nil {
-		log.Fatalf("JSON unmarshaling failed: %s", err)
-		os.Exit(1)
+	err = json.Unmarshal([]byte(body), &comic)
+	if err != nil {
+		if e, ok := err.(*json.SyntaxError); ok {
+			return nil, fmt.Errorf("failed to unmarshal due to syntax error at byte offset %d", e.Offset)
+		}
+		return nil, fmt.Errorf("failed to unmarshal: %v", err)
 	}
-	return comic
+	return &comic, nil
 }
 
-func GetRandomComic() Comic {
-	rand.Seed(time.Now().UnixNano())
-
-	c := GetComic("")
-	n := rand.Intn(c.Number)
-
-	return GetComic(strconv.Itoa(n))
-}
-
-// Parses API path "http://xkcd.com/<comic-num>/info.0.json"
-func getAPIEndpoint(baseUrl, num string) (string, error) {
+func parseEndpoint(baseUrl, num string) (string, error) {
 	u, err := url.Parse(baseUrl)
 	if err != nil {
-		return "", fmt.Errorf("invalid API endpoint")
+		return "", fmt.Errorf("invalid endpoint: %v", err)
 	}
 
-	u.Path = path.Join(u.Path, num, "info.0.json")
+	u.Path = path.Join(u.Path, num, endPoint)
 	return u.String(), nil
 }
 
 func main() {
-	var random = flag.Bool("random", false, "generate random comic")
-	flag.Parse()
-
-	for _, v := range os.Args[1:] {
-		if *random {
-			fmt.Println(GetRandomComic())
-			os.Exit(0)
-		}
-
-		_, err := strconv.ParseInt(v, 10, 0)
-		if err != nil {
-			log.Fatalf("Input must be integer")
-			os.Exit(1)
-		}
-
-		fmt.Println(GetComic(v))
+	start := time.Now()
+	comics, err := GetAllComics(2000)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	fmt.Printf("Time taken: %v\n", time.Since(start))
+	fmt.Println(len(comics))
 }
