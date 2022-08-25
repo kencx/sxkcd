d71058dd8504132661f4bae0ca3f6a6fa7d97e29
kencx <kennethcheo@pm.me>
2022-08-26 00:47:06 +0800

    fix entrypoint.sh requiring redis uri

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -f "comics.json" ]; then
-	./rkcd server -p 6380 -f comics.json
+	./rkcd server -p 6380 -r redis:6379 -f comics.json
 else
-	./rkcd server -p 6380 -f https://rkcd.s3.ap-southeast-1.amazonaws.com/comics.json
+	./rkcd server -p 6380 -r redis:6379 -f https://rkcd.s3.ap-southeast-1.amazonaws.com/comics.json
 fi
