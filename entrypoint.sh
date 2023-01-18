0a46c2b9b5c44d909bdddb09ff40942f56d78f12
kencx <kennethcheo@pm.me>
2023-01-19 00:05:35 +0800

    exec sxkcd process in entrypoint script
    This starts sxkcd as PID 1, allowing it to receive SIGTERM and perform
    graceful shutdown properly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -f "/data/comics.json" ]; then
-	./sxkcd server -p 6380 -r redis:6379 -f /data/comics.json
+	exec ./sxkcd server -p 6380 -r redis:6379 -f /data/comics.json
 else
-	./sxkcd server -p 6380 -r redis:6379 -f https://sxkcd.s3.ap-southeast-1.amazonaws.com/comics.json
+	exec ./sxkcd server -p 6380 -r redis:6379 -f https://sxkcd.s3.ap-southeast-1.amazonaws.com/comics.json
 fi
--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -9,9 +9,12 @@ docker run --rm --entrypoint ./sxkcd \
 
 sleep 3
 
+SECONDS=0
 if [ $(find "$SXKCD_PATH/app_data" -mtime -1 -type f -name "comics.json" 2>/dev/null) ];
 then
 	echo "Restarting sxkcd..."
 	docker compose -f "$SXKCD_PATH/docker-compose.yml" restart
 fi
+DURATION=$SECONDS
+echo "Restarted sxkcd in $((DURATION % 60)) seconds"
 
